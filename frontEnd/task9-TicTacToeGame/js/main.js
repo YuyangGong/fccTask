51d561b7745e0d7718be8263f7ec7830ae2f9ddd
yuyanggong <421736079@qq.com>
2016-12-05 22:43:51 +0800

    js complete

--- frontEnd/task9-TicTacToeGame/js/main.js
+++ frontEnd/task9-TicTacToeGame/js/main.js
@@ -40,28 +40,74 @@
 			viewObj.playerNum = isOnePlayer? 'one' : 'two';
 			viewObj.symbol = isOSymbol? 'O' : 'X';
 			viewObj.curPlayer = 'one';
-			console.log(view.symbol)
 		},
 		go: function(target) {
 			var viewObj = view,
-				index = [].indexOf.call(tableTd, target);
+				index = [].indexOf.call(tableTd, target), computerIndex, winner;
 			if(viewObj.curPlayer === 'one') {
 				viewObj.curPlayer = 'two';
-				viewObj.chessboard[index] = viewObj.symbol === 'O' ? 'O' : 'X'; 
+				viewObj.chessboard[index] = viewObj.symbol === 'O' ? 'O' : 'X';
+				if(viewObj.playerNum === 'one' && (computerIndex = computerGO()) !== null) tableTd[computerIndex].click();
 			}
 			else {
 				viewObj.curPlayer = 'one';
 				viewObj.chessboard[index] = viewObj.symbol === 'O' ? 'X' : 'O'; 
 			}
 			this.updateView();
+			winner = judge.isEnd();
+    			if(winner) {
+    				alert(winner);
+    				controller.reset();
+    		}
 		}
 	}
+
+	function computerGO() {
+		var viewObj = view,
+			symbol = viewObj.symbol,
+			mySymbol = symbol === 'X' ? 'O' : 'X',
+			board = viewObj.chessboard,
+			remain,
+			mid,
+			index,
+			count, 
+			i;
+		var win = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6]
+		];
+		for(i = 0; i < win.length; i++) {
+			mid = win[i];
+			count = mid.filter(function(vi) {return board[vi] === mySymbol}).length;
+			if(count === 2 && mid.some(function(vi) {return board[vi] === '0' })) {
+				return mid.filter(function(vi) {return board[vi] === '0'})[0];
+			}
+		}
+		for(i = 0; i < win.length; i++) {
+			mid = win[i];
+			count = mid.filter(function(vi) {return board[vi] === symbol}).length;
+			if(count === 2 && mid.some(function(vi) {return board[vi] === '0' })) {
+				return mid.filter(function(vi) {return board[vi] === '0'})[0];
+			}
+		}
+		mid = '402681357';
+		for(i = 0; i < mid.length; i++)
+			if(board[mid[i]] === '0') return mid[i];
+		return null;
+	} 
 
 	var judge = {
 		isEnd: function() {
 			var viewObj = view,
 				board = viewObj.chessboard,
-				str = board.join('').replace(/(.{3})/g, '$1-');
+				str = board.join('').replace(/(.{3})/g, '$1-'),
+				resIndex = [];
 			if(/XXX|X..X..X|X...X...X|X....X....X/.test(str)) {
 				return 'WINNER : Player' + (viewObj.symbol === 'X' ? 1 : 2); 
 			}
@@ -80,11 +126,6 @@
     	if(target.nodeName.toLowerCase() === 'td' && viewObj.start) {
     		if(target.innerHTML === '') {
     			controller.go(target);
-    			winner = judge.isEnd();
-    			if(winner) {
-    				alert(winner);
-    				controller.reset();
-    			}
     		}
     	}
     });
