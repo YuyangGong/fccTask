03ce659c0619d0d3fad0556f903f5a16ff023499
yuyanggong <421736079@qq.com>
2016-12-09 19:25:07 +0800

    DV-task03

---
+++ DataVisualization/task3-RecipeBox/js/main.js
@@ -0,0 +1,10 @@
+;(function(window, document,undefined){
+
+})(window, document);
+    debugger;
+    var obj = {
+        a: 1
+    };
+    console.log(obj);
+    obj.b = 222;
+    console.log(obj);
