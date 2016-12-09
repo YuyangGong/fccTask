1c9e29519ab35885f09f383fe85b3f6225ff8922
yuyanggong <421736079@qq.com>
2016-12-09 11:35:40 +0800

    DV-task01 complete

--- DataVisualization/task1-MarkdownPreviewer/js/main.js
+++ DataVisualization/task1-MarkdownPreviewer/js/main.js
@@ -8,8 +8,15 @@
       }
     });
 
-    textarea.addEventListener('keyup', function(e) {
+    function render() {
         var str = marked(textarea.value);
         content.innerHTML = str;
+    }
+
+    textarea.addEventListener('keyup', function(e) {
+        render();
     })
+
+    textarea.innerHTML = "\n# H1\n## H2\n### H3\nAlternatively, for H1 and H2, an underline-ish style:\nAlt-H1\n======\nAlt-H2\n------\nEmphasis, aka italics, with *asterisks* or _underscores_.\nStrong emphasis, aka bold, with **asterisks** or __underscores__.\nCombined emphasis with **asterisks and _underscores_**.\n\nStrikethrough uses two tildes. ~~Scratch this.~~\n## code:\n```js\nvar str = 'Hello World!';\nconsole.log(str);````";
+    render();
 })(window, document);
