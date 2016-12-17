c4cb081b53170d29bd64380b1bb1a8f993fc9e45
yuyanggong <421736079@qq.com>
2016-12-17 23:41:54 +0800

    task5

--- DataVisualization/task5-DungeonGame/js/main.js
+++ DataVisualization/task5-DungeonGame/js/main.js
@@ -1,4 +1,23 @@
 ;(function(window, document, undefined) {
 	'use strict';
+	var container = document.querySelector('.container');
+
+	var model = {
+		player: null,
+		floor: 1
+	};
+
+	var controller = {
+
+	};
+
+	function Player(name, level, health, attack, weapon) {
+		this.name = name || "Anonymou";
+		this.level = level || 0;
+		this.health = health || 100;
+		this.attack = attack || 10;
+		this.weapon = weapon || 0;
+		this.exp = 0;
+	}
 
 })(window, document);
