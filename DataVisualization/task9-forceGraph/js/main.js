d5bce622ea1f300e43f0a2dd169ff8e72bd8b92f
yuyanggong <421736079@qq.com>
2016-12-20 21:17:11 +0800

    task9 start

--- DataVisualization/task9-forceGraph/js/main.js
+++ DataVisualization/task9-forceGraph/js/main.js
@@ -31,69 +31,11 @@
 		}
 	}
 
-	function getMonth(str) {
-		var monthMap = ['Janurary', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-		return monthMap[str - 1];
-	}
-
-	var colorSet = new Array(12).join('-').split('-').map(function(_, i) {
-			return "rgb(" + (i * 15) + ", " + (i*25) + ", " + (i*20) + ")";
-		});
-
-	function getColor(num) {
-		var rangeSet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-			i = colorSet.length;
-		while(i--) {
-			if(num > rangeSet[i]) break;
-		}
-		return colorSet[i] || colorSet[i + 1];
-	}
-
 	ajax({
 		address: "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json",
 		callback: function(respond) {
-			var data = respond.monthlyVariance,
-				len = data.length,
-				baseTemperature = respond.baseTemperature,
-				res = "",
-				temper,
-				i;
-			for(i = 0; i < len; i++) {
-				temper = (baseTemperature + data[i].variance).toFixed(3);
-				res += "<div style='top:" + (i%12*35) + "px;left:" + ((i/12|0)*4) + "px;background-color:" + (getColor(+temper)) + "'>" + 
-					   "<span><h5>" + (data[i].year) + " - " + (getMonth(data[i].month)) + "</h5><h6>" + temper + " C°</h6>" + (data[i].variance) + " C°</span></div>";
-			}
-			graph.innerHTML = res;
+			
 		}
 	});
 
-	function paintColorBlock() {
-		var colorBlock = document.querySelector('.color-block'),
-			res = "",
-			i;
-		console.log(colorBlock)
-		for(i = 0; i < 12; i++) {
-			res += "<div style='left:" + (i*30) + "px;background-color:" + (colorSet[i]) + "'><span>" + i + "</span></div>";
-		}
-		colorBlock.innerHTML = res;
-	}
-
-	function paintNum() {
-		var xLine = document.querySelector('.x-line'),
-			yLine = document.querySelector('.y-line'),
-			xRes = '',
-			yRes = '',
-			i;
-		for(i = 0; i < 26; i++) {
-			xRes += "<div style='right:" + (7 + i * 40) + "px'>" + (2010 - i*10) + "</div>";
-		}
-		for(i = 0; i < 12; i++) {
-			yRes += "<div style='top:" + (10 + i*35) + "px'>" + getMonth(i + 1) + "</div>";
-		}
-		xLine.innerHTML = xRes;
-		yLine.innerHTML = yRes;
-	}
-
-	paintColorBlock();
-	paintNum();
 })(window, document);
