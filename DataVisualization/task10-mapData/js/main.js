c72495365814da6fc27e9bb587ffe1ea2cf2502b
yuyanggong <421736079@qq.com>
2016-12-24 12:42:24 +0800

    task10

--- DataVisualization/task10-mapData/js/main.js
+++ DataVisualization/task10-mapData/js/main.js
@@ -1,4 +1,45 @@
 ;~function(window, document, undefined) {
 	'use strict';
 
+	var container = document.querySelector(".container"),
+		graph = document.querySelector(".graph");
+	
+	function ajax(arg) {
+		var require, key, header = arg.header;
+		if(window.XMLHttpRequest) {
+		  require = new XMLHttpRequest();
+		}
+		else if(window.ActiveXObject) {
+		  require = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		if(require) {
+			require.onreadystatechange = function() {
+				if(require.readyState == 4 && require.status == 200) {
+					var obj = JSON.parse(require.responseText);
+					if(arg.callback) arg.callback(obj);
+				}
+			};
+			require.open(arg.type || "GET", 
+				      arg.address,
+				      true);
+			if(header) {
+				for(key in header) {
+					require.setRequestHeader(key, header[key]);
+				}
+			}
+	        require.send();
+		}
+	}
+
+	ajax({})
+
+	ajax({
+		address: "https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json",
+		callback: function(respond) {
+			 generateGraph(respond);
+			 generateLines(respond);
+		}	
+	});
+
+
 }(window, document);
