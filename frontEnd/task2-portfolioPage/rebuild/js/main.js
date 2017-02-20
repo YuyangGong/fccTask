822deac1b836baa46461dba11f1ef5e1cdd02211
yuyanggong <421736079@qq.com>
2017-02-20 15:21:33 +0800

    rebuild frontEnd Task2 use jquery in rebuild file

---
+++ frontEnd/task2-portfolioPage/rebuild/js/main.js
@@ -0,0 +1,61 @@
+;( (window, document, undefined) => {
+	
+
+	let navBars = $("#nav-bars"),
+		nav = $("#nav"),
+		archArr = nav.find("a"),
+		heightArr,
+		scrollTimer;
+		
+
+	// method for update tags height when change window size
+	let updateHeight = _ => heightArr = ['contactTag', 'portfolioTag' , 'topTag'].map(v => $(`#${v}`).position().top|0);
+    
+    updateHeight();
+
+    // toggle navbar at small device, as phone, ipad.	
+	navBars.click(_ => nav.toggle());
+
+	// move to the tag position
+	nav.click( e => {
+
+	    let target = e.target,
+	    	curHeight,
+	    	tarHeight;
+
+	    if(target.nodeName.toLowerCase() == "a"){
+	      // if the timer already exist, clear it.
+	  	  if(scrollTimer) window.clearTimeout(scrollTimer);  	
+	  	  // get the height of archer
+	      tarHeight = heightArr[2-archArr.index(target)];
+
+	      scrollTimer = setInterval(_ => {
+	      	curHeight = $(window).scrollTop();
+	      	if(curHeight === tarHeight) clearTimeout(scrollTimer);
+	      	else if(tarHeight > curHeight) $(window).scrollTop(curHeight + Math.ceil((tarHeight - curHeight) / 10));
+	        else $(window).scrollTop(curHeight - Math.ceil((curHeight - tarHeight) / 10));
+	      }, 30);
+	    }
+	    return false;
+	});
+	
+	
+	$(window).on('scroll', e => {
+		lastTime = new Date();
+		const changeActive = i => {
+			archArr.removeClass('active');
+		    archArr.eq(i).addClass('active');
+		    return true;
+		}
+
+		let scrollTop = $(window).scrollTop();
+
+		heightArr.some((v, i) => scrollTop >= v && changeActive(2-i));
+
+	});
+	
+
+	$(window).on('resize', v => updateHeight());
+
+
+})(window, document);
